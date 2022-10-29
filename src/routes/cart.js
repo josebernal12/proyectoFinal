b12739fd48915440cb8d099444ac2aef7f75f369
jose <jose12_bernal@hotmail.com>
2022-10-29 17:28:48 -0600

    Validation route cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { createCart, deleteCart, getCart, getCartById, uploadCart } from '../controllers/cart.controller.js'
+import { esAdminRole } from '../middlewares/role.js'
+import { validJWTPostman } from '../middlewares/valid-jwt-postman.js'
 const router = Router()
 
 router.get('/', getCart)
 router.get('/:id', getCartById)
-router.post('/', createCart)
-router.put('/:id', uploadCart)
-router.delete('/:id', deleteCart)
+router.post('/', validJWTPostman, esAdminRole, createCart)
+router.put('/:id', validJWTPostman, esAdminRole, uploadCart)
+router.delete('/:id', validJWTPostman, esAdminRole, deleteCart)
 export default router
